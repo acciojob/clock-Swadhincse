78eb78788ef2c13472c5b28d99a38b87ab58a7b6
Swadhin Ghosh <43653792+Swadhincse@users.noreply.github.com>
2023-05-05 01:31:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function displayDateTime() {
+			const now = new Date();
+			const date = now.toDateString();
+			const time = now.toLocaleTimeString();
+			document.getElementById("timer").innerHTML = date + " " + time;
+		}
+
+		setInterval(displayDateTime, 1000);
